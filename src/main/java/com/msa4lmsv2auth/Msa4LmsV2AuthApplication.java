8f8c9e1e69d16cc991f31771a9d78c9eac28bcdc
jinny-do <sonmojiri7@gmail.com>
2026-08-07 13:05:51 +0900

    first commit

---
+++ src/main/java/com/msa4lmsv2auth/Msa4LmsV2AuthApplication.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4LmsV2AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4LmsV2AuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa4lmsv2auth/Msa4LmsV2AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4LmsV2AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
